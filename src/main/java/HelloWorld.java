8b7355d8ef41c1ed6f811ab9d4020bd001552a88
HOME-PC\Floppa <amelie1908@inbox.ru>
2024-05-04 22:46:32 +0300

    Добавил 5,6,7 задания

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.response.Response;
 
@@ -11,5 +13,44 @@ public class HelloWorld {
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
+    }
+
+    @Test
+    public void testParsingJson() {
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String answer = response.get("messages.message[1]");
+        System.out.println(answer);
+    }
+
+    @Test
+    public void testRedirectFalse() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String header = response.getHeader("location");
+        System.out.println(header);
+    }
+
+    @Test
+    public void testRedirectTrue() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String header = response.getHeader("location");
+        System.out.println(header);
     }
 }
