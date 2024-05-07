7b2fd88f2ef88a0d879e85983616ce1cb5c158a8
HOME-PC\Floppa <amelie1908@inbox.ru>
2024-05-07 23:12:19 +0300

    Добавил 3 раздел

---
+++ src/main/java/org/example/Lesson3.java
@@ -0,0 +1,193 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Lesson3 {
+
+    //JUnit и простые тесты
+
+    @Test
+    public void testFor200() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        //assertTrue(response.statusCode() == 200, "Unexpected status code");
+        assertEquals(200, response.statusCode(), "Unexpected status code");
+    }
+
+    @Test
+    public void testFor404() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2")
+                .andReturn();
+        assertEquals(404, response.statusCode(), "Unexpected status code");
+    }
+
+    //Параметризованные тесты
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
+    public void testParamsName(String name) {
+        Map<String, String> queryParams = new HashMap<>();
+
+        if (name.length() > 0) {
+            queryParams.put("name", name);
+        }
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length() > 0) ? name : "someone";
+        assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
+    }
+
+    // Позитивный тест на авторизацию
+
+    @Test
+    public void testPositiveAuthUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+        int userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+
+        assertEquals(200, responseGetAuth.statusCode(), "Unexpected status code");
+        assertTrue(cookies.containsKey("auth_sid"), "Response doesn't have 'auth_sid' cookie");
+        assertTrue(headers.hasHeaderWithName("x-csrf-token"), "Response doesn't have 'x-csrf-token' header");
+        assertTrue(responseGetAuth.jsonPath().getInt("user_id") > 0, "User id should be greater than 0");
+
+        JsonPath responseCheckAuth = RestAssured
+                .given()
+                .header("x-csrf-token", responseGetAuth.getHeader("x-csrf-token"))
+                .cookie("auth_sid", responseGetAuth.getCookie("auth_sid"))
+                .get("https://playground.learnqa.ru/api/user/auth")
+                .jsonPath();
+
+        int userIdOnCheck = responseCheckAuth.getInt("user_id");
+        assertTrue(userIdOnCheck > 0, "Unexpected user id " + userIdOnCheck);
+
+        assertEquals(
+                userIdOnAuth,
+                userIdOnCheck,
+                "user id from auth request is now equal to user_id from check request"
+        );
+    }
+
+    // Негативные тесты на авторизацию
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "headers"})
+    public void testNegativeAuthUser(String condition) {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
+
+        if (condition.equals("cookie")) {
+            spec.cookie("auth_sid", cookies.get("auth_sid"));
+        } else if (condition.equals("headers")) {
+            spec.header("x-csrf-token", headers.get("x-csrf-token"));
+        } else {
+            throw new IllegalArgumentException("Condition value is known: " + condition);
+        }
+
+        JsonPath responseForCheck = spec.get().jsonPath();
+        assertEquals(0, responseForCheck.getInt("user_id"), "user_id should be 0 for unauth request");
+    }
+
+    //Функция BeforeEach() в JUnit
+
+    String cookie;
+    String header;
+    int userIdOnAuth;
+
+    @BeforeEach
+    public void testLoginUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        this.cookie = responseGetAuth.getCookie("auth_sid");
+        this.header = responseGetAuth.getHeader("x-csrf-token");
+        this.userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+    }
+
+    @Test
+    public void testAuthUser() {
+        JsonPath responseCheckAuth = RestAssured
+                .given()
+                .header("x-csrf-token", this.header)
+                .cookie("auth_sid", this.cookie)
+                .get("https://playground.learnqa.ru/api/user/auth")
+                .jsonPath();
+
+        int userIdOnCheck = responseCheckAuth.getInt("user_id");
+        assertTrue(userIdOnCheck > 0, "Unexpected user id " + userIdOnCheck);
+
+        assertEquals(
+                userIdOnAuth,
+                userIdOnCheck,
+                "user id from auth request is now equal to user_id from check request"
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "headers"})
+    public void testParamsAuthUser(String condition) {
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
+
+        if (condition.equals("cookie")) {
+            spec.cookie("auth_sid", this.cookie);
+        } else if (condition.equals("headers")) {
+            spec.header("x-csrf-token", this.header);
+        } else {
+            throw new IllegalArgumentException("Condition value is known: " + condition);
+        }
+
+        JsonPath responseForCheck = spec.get().jsonPath();
+        assertEquals(0, responseForCheck.getInt("user_id"), "user_id should be 0 for unauth request");
+    }
+}
